3d4d8b6aa5d3a503fe259499a637e7a9dcf13c4b
Billy Stewart <stewartb774@gmail.com>
2024-06-08 21:58:57 -0400

    Initialize power rankings table with dummy data

---
+++ script.js
@@ -0,0 +1,21 @@
+const NUM_TEAMS = 12;
+const NUM_COLS = 9;
+
+const initializeTable = () => {
+    tblBody = document.getElementById("power-rankings-tbody")
+    for (let i = 0; i < NUM_TEAMS; i++) {
+        // creates a table row
+        const row = document.createElement("tr");
+
+        for (let j = 0; j < NUM_COLS; j++) {
+            const cell = document.createElement("td");
+              const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+              cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+
+        tblBody.appendChild(row);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', initializeTable)
